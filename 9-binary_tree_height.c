ac10ba4e78f19323d958b6cdd084d993af947800
dantsub <dany.mrtinez@gmail.com>
2020-03-11 15:58:40 -0500

    add task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - goes through a binary tree using pre-order traversal
+ * @tree: parent of node.
+ * Return: nothing.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h_left, h_right;
+
+	if (!tree)
+		return (0);
+	h_left = tree->left ? binary_tree_height(tree->left) : 0;
+	h_right = tree->right ? binary_tree_height(tree->right) : 0;
+	return (h_left > h_right ? h_left : h_right);
+}
