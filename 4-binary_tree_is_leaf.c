cac9097568828e89de7e78023ce314dd18eaecde
auraPasmin <1288@holbertonschool.com>
2020-03-11 14:41:13 -0500

    fix 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,6 @@
 #include "binary_trees.h"
 /**
  * binary_tree_is_leaf - that checks if a node is a leaf
- *
  * @node: parent of node.
  * Return: one or cero.
  */
@@ -13,5 +12,6 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 	}
 	if (!node->letf && !node->right)
 		return (1);
-	return (0);
+	else
+		return (0);
 }
