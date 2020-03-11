9be1f2f67a9fe254272125ef9fe1eaf41fc7ba8f
dantsub <dany.mrtinez@gmail.com>
2020-03-11 16:48:11 -0500

    update header, fix 11 docs, add task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - goes through a binary tree using pre-order traversal
+ * @tree: parent of node.
+ * Return: nothing.
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && tree->right)
+		return (1);
+	leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (leaves);
+}
