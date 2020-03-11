7d44192f22a27e6d85084de4a345d66206de9c97
dantsub <dany.mrtinez@gmail.com>
2020-03-11 16:00:17 -0500

    forget +1

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	h_left = tree->left ? binary_tree_height(tree->left) : 0;
-	h_right = tree->right ? binary_tree_height(tree->right) : 0;
+	h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (h_left > h_right ? h_left : h_right);
 }
